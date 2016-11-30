b3f7690b5745e2cdaabf52a02c92da4849d54792
moross15 <morgan.ross@teamaol.com>
2016-11-30 12:46:39 -0800

    roll dice takes sides arg

--- roll_dice.php
+++ roll_dice.php
@@ -1,11 +1,15 @@
 <?php
 echo "<input type='button' onclick='AjaxRequest();' value='Roll a d6'/><br />";
-echo "you rolled a " . roll_d6();
+echo "you rolled a " . roll_die(6);
 
 function roll_d6() {
 	
 	return rand(1, 6);
 }
 
+function roll_die($sides)
+{
+	return rand(1, $sides);
+}
 
 ?>
