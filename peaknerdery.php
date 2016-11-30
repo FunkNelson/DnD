0f7f485a249e732c908daf568175600f6ed76e6f
moross15 <morgan.ross@teamaol.com>
2016-11-30 12:00:06 -0800

    add js button

--- peaknerdery.php
+++ peaknerdery.php
@@ -2,23 +2,26 @@
 <head>
 	<title>Peak Nerdery</title>
 	<meta id="meta" name="viewport" content="width=device-width; initial-scale=1.0" />
+	<script type='text/javascript' src='Ajax_Example_JavaScript.js'></script>
 </head>
 <body>
-
-	<!-- Profile 1 -->
+	<h1 align="center">Peak Nerdery</h1><br />
 	
-
+	<div id='AjaxResponse'>
+		<input type='button' onclick='AjaxRequest();' value='DERP'/>
+    </div>
+	
+	
+	<?php
+	require_once('profile_strip.php');
+	$party = array(1, 2, 3, 4);
+	
+	foreach ($party as $hero) 
+	{
+		print_profile($hero);
+	}
 		
-		<?php
-		require_once('profile_strip.php');
-		$party = array(1, 2, 3, 4);
-		
-		foreach ($party as $hero) 
-		{
-			print_profile($hero);
-		}
-			
-		?>
+	?>
 		
 		
 	
